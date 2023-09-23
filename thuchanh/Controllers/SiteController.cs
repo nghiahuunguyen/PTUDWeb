7602042b9e10c4745b29a65641b09412f57aa4b6
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2023-09-23 10:30:09 +0700

    thuchanh

---
+++ thuchanh/Controllers/SiteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace thuchanh.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Sie
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
