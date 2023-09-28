71ada97532ced0c3f8e7975c7eb34287c0288af9
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2023-09-28 10:21:26 +0700

    buoi2

---
+++ thuchanh1/thuchanh/Controllers/SiteController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.Model;
+
+namespace thuchanh.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Sie
+        public ActionResult Index()
+        {
+            MyDBContext db = new MyDBContext();//tao moi DB
+            int sodong = db.Products.Count();
+            ViewBag.sodong = sodong;
+            return View();
+        }
+    }
+}
