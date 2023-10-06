26a1eb069c21ebc3f32f3be9facebd1a4af9128a
nghiahuunguyen <145564918+nghiahuunguyen@users.noreply.github.com>
2023-10-06 08:52:34 +0700

    Buổi 3

---
+++ thuchanh/MyClass/DAO/CategoriesDAO.cs
@@ -0,0 +1,81 @@
+﻿using MyClass.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace MyClass.DAO
+{
+
+    public class CategoriesDAO
+    {
+        private MyDBContext db = new MyDBContext();
+        //INDEX
+        public List<Categories> getList()
+        {
+            return db.Categories.ToList();
+        }
+
+        //Hien thi danh sach theo trang thai
+        public List<Categories> getList(string status = "All")
+        {
+            List<Categories> list = null;
+            switch (status)
+            {
+                case "Index":
+                    {
+                        list = db.Categories
+                            .Where(m => m.Status != 0)
+                            .ToList();
+                        break;
+                    }
+                case "Trash":
+                    {
+                        list = db.Categories
+                           .Where(m => m.Status == 0)
+                           .ToList();
+                        break;
+                    }
+                default:
+                    {
+                        list = db.Categories.ToList();
+                        break;
+                    }
+            }
+            return list;
+        }
+
+        public Categories getRow(int? id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+            else
+            {
+                return db.Categories.Find(id);
+            }
+
+        }
+        public int Insert(Categories row)
+        {
+            db.Categories.Add(row);
+            return db.SaveChanges();
+        }
+        public int Update(Categories row)
+        {
+            db.Entry(row).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+        public int Delete(Categories row)
+        {
+            db.Categories.Remove(row);
+            return db.SaveChanges();
+        }
+
+
+
+    }
+}
---
+++ thuchanh/thuchanh/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyClass.DAO;
+using MyClass.Model;
+namespace thuchanh.Areas.Admin.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        CategoriesDAO categoriesDAO = new CategoriesDAO();
+
+        // GET: Admin/Categories/Index
+        public ActionResult Index()
+        {
+            return View(categoriesDAO.getList("Index"));
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categories.CreateAt = DateTime.Now;
+                categories.UpdateAt = DateTime.Now;
+                categoriesDAO.Insert(categories);
+                return RedirectToAction("Index");
+            }
+
+            return View(categories);
+        }
+
+
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Categories categories)
+        {
+            if (ModelState.IsValid)
+            {
+                categoriesDAO.Update(categories);
+                return RedirectToAction("Index");
+            }
+            return View(categories);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Categories categories = categoriesDAO.getRow(id);
+            if (categories == null)
+            {
+                return HttpNotFound();
+            }
+            return View(categories);
+        }
+
+        // POST: Admin/Category/Delete/5:Xoa mot mau tin ra khoi CSDL
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categories categories = categoriesDAO.getRow(id);
+            categoriesDAO.Delete(categories);
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
